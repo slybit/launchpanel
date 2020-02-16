29f1b130cc5352eee414cd12fec074c0d197f1cb
Stefaan Seys <slybit@bluemalt.tech>
2020-02-16 21:13:04 +0100

    first commit

---
+++ src/Panel.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+
+export class Panel extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cards: [],
+        };
+    }
+
+    loadCardsFromServer = () => {
+        axios.get('/cards.json')
+            .then((response) => {
+                this.setState(response.data);
+            })
+            .catch((error) => {
+                alert("Cannot read card details from server." + "\n\n" + error);
+            });
+    }
+
+    componentDidMount() {
+        this.loadCardsFromServer();
+    }
+
+    render() {
+        const listItems = this.state.cards.map((card) =>
+
+            <div key={card.title} className="col-auto mb-4">
+                <a href={card.url} >
+                    <div className="card panel-card" style={{ color: card.color}} >
+                        <div className="card-body">
+                            <h5 className="card-title"><span class={"mdi " + card.icon + " h2"} >  </span>{card.title}</h5>
+                            <p className="card-text">{card.description}</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+        );
+
+        return (
+            <div>
+                <div class="jumbotron">
+                    <h1><span class={"mdi " + this.state.icon + " h1"}></span> {this.state.title}</h1>
+                    <h5 className="text-muted">{this.state.description}</h5>
+                </div>
+                <div className="container mt-4">
+                    <div className="row">
+                        {listItems}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+
+
+}
